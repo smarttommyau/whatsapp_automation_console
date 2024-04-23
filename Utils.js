4714306e8dde48890d5f32ee22a013e310d7187b
smarttommyau <smarttommyau@gmail.com>
2024-04-23 21:00:30 +0800

    Use media Key as identifier for media to prevent repeat downloading

--- Utils.js
+++ Utils.js
@@ -176,20 +176,21 @@ export async function generateMessageJson(msg,contact,chat,path){
     }
     if(isMedia){
         const media = await msg.downloadMedia();
-
+        
         //save media
         let filename = "";
         if(msg.type == MessageTypes.STICKER){
            filename += "sticker_";
         }
+        const mediaKey = msg.mediaKey.replaceAll('/','_').replaceAll('=','').replaceAll('+','_');
         if(media.filename){
-            filename += msg.id.id + "_" + media.filename;
+            filename += (mediaKey||msg.id.id) + "_" + media.filename;
         }else{
-            filename += msg.id.id + "." + mime.extension(media.mimetype);
+            filename += (mediaKey||msg.id.id) + "." + mime.extension(media.mimetype);
         }
         fs.writeFileSync(path + filename,Buffer.from(media.data,'base64'));
         jsonout['Media'] = {
-            'filename':media.filename
+            'filename':filename
         }
     }
     jsonout['Body'] = msg.body;
